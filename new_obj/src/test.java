f7e14c365c3796d8812f1274778250e1e23850f4
wp <1115003086@qq.com>
2019-03-20 10:02:38 +0800

    创建项目

---
+++ new_obj/src/test.java
@@ -0,0 +1,13 @@
+/** 
+ * @author  作者 E-mail:
+ * @date 创建时间：2019-3-20 上午9:58:37
+ * @version 1.0
+ * @parameter
+ * @since
+ * @return 
+ */
+public class test {
+	public static void main(String[] args) {
+		System.out.println("123");
+	}
+}
